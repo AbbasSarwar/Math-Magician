5bcc22119f2b73dab5afe964e41b29b12482d233
Abbas Sarwar <abbassarwar100@gmail.com>
2023-04-27 21:05:14 +0500

    Error state added

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,29 +2,45 @@ import { useEffect, useState } from 'react';
 
 const Quotes = () => {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch('https://api.api-ninjas.com/v1/quotes?category=happiness', {
-        method: 'GET',
-        headers: {
-          'X-Api-Key': '9EoCxmipJCj+zR35ZNsY3A==93CN5SnCNPUiktDW',
-          'Content-type': 'application/json',
-        },
-      });
-      const data = await res.json();
-      if (data.length > 0) {
-        setData(data[0].quote);
-      } else {
-        setData('');
+      try {
+        const res = await fetch('https://api.api-ninjas.com/v1/quotes?category=happiness', {
+          method: 'GET',
+          headers: {
+            'X-Api-Key': '9EoCxmipJCj+zR35ZNsY3A==93CN5SnCNPUiktDW',
+            'Content-type': 'application/json',
+          },
+        });
+        const data = await res.json();
+        if (data.length > 0) {
+          setData(data[0].quote);
+        } else {
+          setData('');
+        }
+      } catch (err) {
+        setError(err.message);
       }
     };
     fetchData();
   }, []);
+
+  if (error) {
+    return (
+      <div className="Quotes">
+        <p>
+          Error:
+          {error}
+        </p>
+      </div>
+    );
+  }
+
   return (
     <div className="Quotes">
-      {data ? (<p>{data}</p>) : (
-        <p>Loading....</p>
-      )}
+      {data ? <p>{data}</p> : <p>Loading....</p>}
     </div>
   );
 };
