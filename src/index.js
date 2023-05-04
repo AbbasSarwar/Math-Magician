b6e5eab4eedae88866f11c7aeac8aa78ec6a01a8
Abbas Sarwar <abbassarwar100@gmail.com>
2023-05-04 12:59:54 +0500

    Ready for gh-pages

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Math-magicians">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
